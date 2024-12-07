00a368e0e05ac61ef9525df917c8e2ce2c9660e9
Franklin Bello Romero <codetancy@gmail.com>
2024-12-07 02:13:50 -0500

    Solving Day6 Part1
    First time using the Array2D.
    
    I used an active pattern to match if the cell is within bounds, which I learned from Day3.
    
    Learned that Array2D mutations are in place.
    
    My first instinct was to map each symbol to an enum. However, I remembered (from when I used to do Leetcode) that I could denote a visited cell with an int, and keep adding to the counter every time it is visited.

--- Common/Library.fs
+++ Common/Library.fs
@@ -1,5 +1,7 @@
 ﻿namespace Common
 
+open Microsoft.FSharp.Core
+
 module Option =
     let toOption value bool =
         match bool with
@@ -23,16 +25,50 @@ module List =
         if List.length prefix > List.length list then
             false
         else
-           List.take (List.length prefix) list = prefix
-    
+            List.take (List.length prefix) list = prefix
+
     let after prefix list =
         if startsWith prefix list then
             List.skip (List.length prefix) list
         else
             failwith "List must start with prefix"
-            
+
     let tryAfter prefix list =
         if startsWith prefix list then
             Some <| List.skip (List.length prefix) list
         else
             None
+
+[<RequireQualifiedAccess>]
+module Array2D =
+
+    let mapAt index1 index2 transform (array: 'T[,]) =
+        array[index1, index2] <- transform array[index1, index2]
+
+    let find value (array: 'T[,]) =
+        let rows = Array2D.length1 array
+        let cols = Array2D.length2 array
+
+        let indices =
+            seq {
+                for i in 0 .. rows - 1 do
+                    for j in 0 .. cols - 1 do
+                        if array[i, j] = value then
+                            yield i, j
+            }
+
+        indices |> Seq.head
+
+    let count predicate (array: 'T[,]) =
+        let rows = Array2D.length1 array
+        let cols = Array2D.length2 array
+
+        let values =
+            seq {
+                for i in 0 .. rows - 1 do
+                    for j in 0 .. cols - 1 do
+                        yield array[i, j]
+            }
+
+        (0, values)
+        ||> Seq.fold (fun acc el -> if predicate el then acc + 1 else acc)
---
+++ Day06/Program.fs
@@ -0,0 +1,71 @@
+﻿open System
+open System.IO
+
+open Common
+
+module Parser =
+
+    let parseLine line =
+        line
+        |> Array.ofSeq
+        |> Array.map (function
+            | '.' -> 0
+            | '^' -> 1
+            | '#' -> -1
+            | c -> failwith $"Unrecognized character {c}")
+
+    let parse lines = lines |> Seq.map parseLine |> array2D
+
+module S1 =
+
+    type Direction =
+        | NORTH
+        | EAST
+        | SOUTH
+        | WEST
+
+    let offset =
+        function
+        | NORTH -> 0, -1 
+        | EAST -> 1, 0
+        | SOUTH -> 0, 1
+        | WEST -> -1, 0
+
+    let next =
+        function
+        | NORTH -> EAST
+        | EAST -> SOUTH
+        | SOUTH -> WEST
+        | WEST -> NORTH
+
+    let (|InBounds|_|) (floor: int[,]) (y, x) =
+        if
+            (y >= 0 && y < Array2D.length1 floor)
+            && (x >= 0 && x < Array2D.length2 floor)
+        then
+            Some(y, x)
+        else
+            None
+
+    let patrol floor =
+
+        let rec loop (floor: int[,]) (y0, x0) direction =
+            let x, y = offset direction
+
+            match y0 + y, x0 + x with
+            | InBounds floor (y1, x1) ->
+                if floor[y1, x1] = -1 then
+                    loop floor (y0, x0) (next direction)
+                else
+                    do Array2D.mapAt y1 x1 (fun n -> n + 1) floor
+                    loop floor (y1, x1) direction
+            | _ -> floor
+        
+        let y0, x0 = Array2D.find 1 floor
+        loop floor (y0, x0) Direction.NORTH
+
+
+let input = File.ReadAllLines "Files/Floor.txt"
+            
+printfn "%d" (input |> Parser.parse |> S1.patrol |> Array2D.count (fun n -> n > 0))
+
