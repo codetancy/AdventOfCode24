3a51338541974b4b13d45b55c4947cff27a251a2
Franklin Bello Romero <codetancy@gmail.com>
2024-12-08 03:42:32 -0500

    Improving Day6 Part 2
    After switching to a Set, execution is so much faster.

--- Day06/Program.fs
+++ Day06/Program.fs
@@ -16,31 +16,34 @@ module Parser =
 
     let parse lines = lines |> Seq.map parseLine |> array2D
 
+type Direction =
+    | NORTH
+    | EAST
+    | SOUTH
+    | WEST
+
+let offset =
+    function
+    | NORTH -> 0, -1
+    | EAST -> 1, 0
+    | SOUTH -> 0, 1
+    | WEST -> -1, 0
+
+let next =
+    function
+    | NORTH -> EAST
+    | EAST -> SOUTH
+    | SOUTH -> WEST
+    | WEST -> NORTH
+
+type Outcome =
+    | Exited
+    | Loop
+
+type Tile = int * int
+type Step = Tile * Direction
+
 module S1 =
-
-    type Direction =
-        | NORTH
-        | EAST
-        | SOUTH
-        | WEST
-
-    let offset =
-        function
-        | NORTH -> 0, -1
-        | EAST -> 1, 0
-        | SOUTH -> 0, 1
-        | WEST -> -1, 0
-
-    let next =
-        function
-        | NORTH -> EAST
-        | EAST -> SOUTH
-        | SOUTH -> WEST
-        | WEST -> NORTH
-
-    type Outcome =
-        | Exited
-        | Loop
 
     let (|InBounds|_|) (floor: int[,]) (y, x) =
         if
@@ -51,33 +54,33 @@ module S1 =
         else
             None
 
-    let rec private loop (floor: int[,]) trail =
-        let head = List.head trail
-        let tail = List.tail trail
-
-        let y0, x0, direction = head
-
-        if List.contains (y0, x0, direction) tail then
-            floor, List.rev trail, Outcome.Loop
+    let rec private loop (floor: int[,]) (visited: Set<Step>) (step: Step) =
+        
+        if Set.contains step visited then
+            floor, visited, Outcome.Loop
         else
+            let visited = Set.add step visited
+            
+            let (y0, x0), direction = step
             let x, y = offset direction
 
             match y0 + y, x0 + x with
             | InBounds floor (y1, x1) ->
                 if floor[y1, x1] = -1 then
-                    let trail = (y0, x0, next direction) :: tail
-                    loop floor trail
+                    let visited = Set.remove step visited
+                    let newStep = (y0, x0), next direction
+                    loop floor visited newStep
                 else
                     do Array2D.mapAt y1 x1 (fun n -> n + 1) floor
-                    let trail = (y1, x1, direction) :: trail
-                    loop floor trail
-            | _ -> floor, List.rev trail, Exited
+                    let nextStep = (y1, x1), direction
+                    loop floor visited nextStep
+            | _ -> floor, visited, Exited
 
     let patrol floor =
         let floor = Array2D.copy floor
         let y0, x0 = Array2D.find 1 floor
-        let trail = [ y0, x0, Direction.NORTH ]
-        loop floor trail
+        let step = (y0, x0), Direction.NORTH
+        loop floor Set.empty step
 
 module S2 =
     open S1
@@ -99,17 +102,18 @@ let input = File.ReadAllLines "Files/Floor.txt"
 let floor = input |> Parser.parse
 
 // Part 1
-let floor', trail, outcome = input |> Parser.parse |> S1.patrol
-let visited = floor' |> Array2D.count (fun n -> n > 0)
+let floor', visited, outcome = input |> Parser.parse |> S1.patrol
+let visitedCount = floor' |> Array2D.count (fun n -> n > 0)
 
-printfn $"{visited}"
+printfn $"{visitedCount}"
 
 // Part 2
-let obstacles = trail |> List.map (fun (y, x, _) -> y, x) |> Set.ofList
+let obstacles: Set<Tile> = visited |> Set.map fst
+
 let loops =
     floor
     |> S2.paradox obstacles
-    |> Seq.filter (fun outcome -> outcome = S1.Outcome.Loop)
+    |> Seq.filter (fun outcome -> outcome = Outcome.Loop)
     |> Seq.length
     
 printfn $"{loops}"
