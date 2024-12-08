623144291826da24abd7476fef8a6d346f4797b6
Franklin Bello Romero <codetancy@gmail.com>
2024-12-08 03:12:45 -0500

    Solving Day6 Part2
    It took a long time to solve Part2.
    
    I am thinking whether I could use a Set instead of a list to keep track of the visited nodes.

--- Day06/Program.fs
+++ Day06/Program.fs
@@ -26,7 +26,7 @@ module S1 =
 
     let offset =
         function
-        | NORTH -> 0, -1 
+        | NORTH -> 0, -1
         | EAST -> 1, 0
         | SOUTH -> 0, 1
         | WEST -> -1, 0
@@ -38,6 +38,10 @@ module S1 =
         | SOUTH -> WEST
         | WEST -> NORTH
 
+    type Outcome =
+        | Exited
+        | Loop
+
     let (|InBounds|_|) (floor: int[,]) (y, x) =
         if
             (y >= 0 && y < Array2D.length1 floor)
@@ -47,28 +51,65 @@ module S1 =
         else
             None
 
-    let patrol floor =
+    let rec private loop (floor: int[,]) trail =
+        let head = List.head trail
+        let tail = List.tail trail
 
-        let rec loop (floor: int[,]) trail =
-            let y0, x0, direction = List.head trail
+        let y0, x0, direction = head
+
+        if List.contains (y0, x0, direction) tail then
+            floor, List.rev trail, Outcome.Loop
+        else
             let x, y = offset direction
 
             match y0 + y, x0 + x with
             | InBounds floor (y1, x1) ->
                 if floor[y1, x1] = -1 then
-                    let trail = (y0, x0, next direction) :: List.tail trail
+                    let trail = (y0, x0, next direction) :: tail
                     loop floor trail
                 else
                     do Array2D.mapAt y1 x1 (fun n -> n + 1) floor
                     let trail = (y1, x1, direction) :: trail
                     loop floor trail
-            | _ -> floor, trail
-        
+            | _ -> floor, List.rev trail, Exited
+
+    let patrol floor =
+        let floor = Array2D.copy floor
         let y0, x0 = Array2D.find 1 floor
-        let trail = [y0, x0, Direction.NORTH]
+        let trail = [ y0, x0, Direction.NORTH ]
         loop floor trail
 
-let input = File.ReadAllLines "Files/Sample.txt"
-let floor, trail = input |> Parser.parse |> S1.patrol
+module S2 =
+    open S1
 
-printfn $"{floor |> Array2D.count (fun n -> n > 0)}"
+    let paradox obstacles floor =
+        let y0, x0 = Array2D.find 1 floor
+        let obstacles = Set.remove (y0, x0) obstacles
+        let count = Set.count obstacles
+
+        obstacles
+        |> Seq.indexed
+        |> Seq.map (fun (i, (y, x)) ->
+            let floor = Array2D.copy floor
+            do floor[y, x] <- -1
+            let _, _, outcome = patrol floor
+            outcome)
+
+let input = File.ReadAllLines "Files/Floor.txt"
+let floor = input |> Parser.parse
+
+// Part 1
+let floor', trail, outcome = input |> Parser.parse |> S1.patrol
+let visited = floor' |> Array2D.count (fun n -> n > 0)
+
+printfn $"{visited}"
+
+// Part 2
+let obstacles = trail |> List.map (fun (y, x, _) -> y, x) |> Set.ofList
+let loops =
+    floor
+    |> S2.paradox obstacles
+    |> Seq.filter (fun outcome -> outcome = S1.Outcome.Loop)
+    |> Seq.length
+    
+printfn $"{loops}"
