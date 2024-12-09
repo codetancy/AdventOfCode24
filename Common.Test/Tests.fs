0f9d41078ff29c8fd23c5bc7f3ecccfe213478a7
Franklin Bello Romero <codetancy@gmail.com>
2024-12-09 09:26:50 -0500

    Format Tests

--- Common.Test/Tests.fs
+++ Common.Test/Tests.fs
@@ -21,27 +21,31 @@ let ``Test Starts With`` () =
 
     let result = List.startsWith [ '1'; '2'; '3'; '4' ] [ '1'; '2'; '3' ]
     result.Should().Equals(false) |> ignore
-    
+
     let result = List.startsWith [ '1'; '2' ] [ '1'; '3' ]
     result.Should().Equals(false) |> ignore
-    
+
     let result = List.startsWith [] [ '1'; '3' ]
     result.Should().Equals(false) |> ignore
-    
+
     let result = List.startsWith [] []
     result.Should().Equals(false) |> ignore
-    
+
 [<Fact>]
 let ``Test After`` () =
-    
-    let result = List.after ['1'; '2'] ['1'; '2'; '3']
-    result.Should().SequenceEqual(['3']) |> ignore
-    
-    let result = List.after ['1'; '2'] ['1'; '2']
+
+    let result = List.after [ '1'; '2' ] [ '1'; '2'; '3' ]
+    result.Should().SequenceEqual([ '3' ]) |> ignore
+
+    let result = List.after [ '1'; '2' ] [ '1'; '2' ]
     result.Should().SequenceEqual([]) |> ignore
-    
-    let result = List.tryAfter ['1'; '2'] ['1']
+
+    let result = List.tryAfter [ '1'; '2' ] [ '1' ]
     result.Should().BeNone() |> ignore
-    
-    let result = List.tryAfter ['1'; '2'] ['3'; '4']
+
+    let result = List.tryAfter [ '1'; '2' ] [ '3'; '4' ]
     result.Should().BeNone() |> ignore
+
+[<Fact>]
+let ``Learning`` () =
+    ()
