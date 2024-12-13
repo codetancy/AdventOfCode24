bc7522f157f22dccf2966e35cb2689ba712d9f7d
Franklin Bello Romero <codetancy@gmail.com>
2024-12-13 12:03:28 -0800

    Organize Tests in Modules

--- Common.Test/Tests.fs
+++ Common.Test/Tests.fs
@@ -5,47 +5,58 @@ open Xunit
 open Common
 open Faqt
 
-[<Fact>]
-let ``Test Span`` () =
-    let number, rest = List.span Char.IsDigit [ '1'; 'c' ]
+module List =
 
-    number.Should().SequenceEqual([ '1' ]) |> ignore
-    rest.Should().SequenceEqual([ 'c' ]) |> ignore
+    [<Fact>]
+    let ``Test Span`` () =
+        let number, rest = List.span Char.IsDigit [ '1'; 'c' ]
+
+        number.Should().SequenceEqual([ '1' ]) |> ignore
+        rest.Should().SequenceEqual([ 'c' ]) |> ignore
 
 
-[<Fact>]
-let ``Test Starts With`` () =
+    [<Fact>]
+    let ``Test Starts With`` () =
 
-    let result = List.startsWith [ '1'; '2'; '3' ] [ '1'; '2'; '3'; '4' ]
-    result.Should().Equals(true) |> ignore
+        let result = List.startsWith [ '1'; '2'; '3' ] [ '1'; '2'; '3'; '4' ]
+        result.Should().Equals(true) |> ignore
 
-    let result = List.startsWith [ '1'; '2'; '3'; '4' ] [ '1'; '2'; '3' ]
-    result.Should().Equals(false) |> ignore
+        let result = List.startsWith [ '1'; '2'; '3'; '4' ] [ '1'; '2'; '3' ]
+        result.Should().Equals(false) |> ignore
 
-    let result = List.startsWith [ '1'; '2' ] [ '1'; '3' ]
-    result.Should().Equals(false) |> ignore
+        let result = List.startsWith [ '1'; '2' ] [ '1'; '3' ]
+        result.Should().Equals(false) |> ignore
 
-    let result = List.startsWith [] [ '1'; '3' ]
-    result.Should().Equals(false) |> ignore
+        let result = List.startsWith [] [ '1'; '3' ]
+        result.Should().Equals(false) |> ignore
 
-    let result = List.startsWith [] []
-    result.Should().Equals(false) |> ignore
+        let result = List.startsWith [] []
+        result.Should().Equals(false) |> ignore
 
-[<Fact>]
-let ``Test After`` () =
+    [<Fact>]
+    let ``Test After`` () =
 
-    let result = List.after [ '1'; '2' ] [ '1'; '2'; '3' ]
-    result.Should().SequenceEqual([ '3' ]) |> ignore
+        let result = List.after [ '1'; '2' ] [ '1'; '2'; '3' ]
+        result.Should().SequenceEqual([ '3' ]) |> ignore
 
-    let result = List.after [ '1'; '2' ] [ '1'; '2' ]
-    result.Should().SequenceEqual([]) |> ignore
+        let result = List.after [ '1'; '2' ] [ '1'; '2' ]
+        result.Should().SequenceEqual([]) |> ignore
 
-    let result = List.tryAfter [ '1'; '2' ] [ '1' ]
-    result.Should().BeNone() |> ignore
+        let result = List.tryAfter [ '1'; '2' ] [ '1' ]
+        result.Should().BeNone() |> ignore
 
-    let result = List.tryAfter [ '1'; '2' ] [ '3'; '4' ]
-    result.Should().BeNone() |> ignore
+        let result = List.tryAfter [ '1'; '2' ] [ '3'; '4' ]
+        result.Should().BeNone() |> ignore
 
-[<Fact>]
-let ``Learning`` () =
-    ()
+module Seq =
+
+    [<Fact>]
+    let ``Split Sequence`` () =
+        let numbers =
+            seq {
+                for i in 1..10 do
+                    yield if i % 3 = 0 then -1 else i
+            }
+
+        let splitted = Seq.split (fun i -> i < 0) numbers
+        ()
