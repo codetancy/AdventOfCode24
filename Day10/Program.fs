2c6dc17bf8cbe9c7944cc22ebed0818df56fdeb4
Franklin Bello Romero <codetancy@gmail.com>
2024-12-23 07:18:29 -0500

    Consolidating Day10

--- Day10/Program.fs
+++ Day10/Program.fs
@@ -27,9 +27,8 @@ module Direction =
 
 let solve (topographicMap: int[,]) =
 
-    let scoreOf (trailHead: int * int) =
-
-        let mutable reached = Set.empty
+    let reachedFrom (trailHead: int * int) =
+        let mutable reached = List.empty
 
         let rec dfs (y0, x0) (visited: Set<int * int>) =
             let visited = Set.add (y0, x0) visited
@@ -41,43 +40,6 @@ let solve (topographicMap: int[,]) =
 
             let hasNotBeenVisited (y, x) =
                 if Set.contains (y, x) visited then None else Some(y, x)
-
-            let isNext height (y, x) =
-                if topographicMap[y, x] = height + 1 then
-                    Some(y, x)
-                else
-                    None
-
-            match topographicMap[y0, x0] with
-            | 9 -> reached <- Set.add (y0, x0) reached
-            | height ->
-                Direction.Values
-                |> List.map Direction.toOffset
-                |> List.map (fun (y, x) -> y0 + y, x + x0)
-                |> List.choose (fun potentialLocation ->
-                    Some potentialLocation
-                    |> Option.bind isInBounds
-                    |> Option.bind hasNotBeenVisited
-                    |> Option.bind (isNext height))
-                |> List.iter (fun nextLocation -> dfs nextLocation visited)
-
-        dfs trailHead Set.empty
-        Set.count reached
-
-    let ratingOf (trailHead: int * int) =
-
-        let mutable reached = List.empty
-
-        let rec dfs (y0, x0) (visited: List<int * int>) =
-            let visited = (y0, x0) :: visited
-
-            let isInBounds (y, x) =
-                match y, x with
-                | Patterns.InBounds topographicMap _ -> Some(y, x)
-                | _ -> None
-
-            let hasNotBeenVisited (y, x) =
-                if List.contains (y, x) visited then None else Some(y, x)
 
             let isNext height (y, x) =
                 if topographicMap[y, x] = height + 1 then
@@ -98,17 +60,27 @@ let solve (topographicMap: int[,]) =
                     |> Option.bind (isNext height))
                 |> List.iter (fun nextLocation -> dfs nextLocation visited)
 
-        dfs trailHead List.empty
-        List.length reached
+        dfs trailHead Set.empty
+        reached
+
+    let scoreOf trails = Set.ofList trails |> Set.count
+    let ratingOf trails = List.length trails
 
     let trailHeads =
-        topographicMap |> Array2D.toSeq |> Seq.filter (fun (_, height) -> height = 0)
+        topographicMap
+        |> Array2D.toSeq
+        |> Seq.filter (fun (_, height) -> height = 0)
+        |> Seq.map fst
 
     trailHeads
-    |> Seq.map (fun (trailHead, _) -> scoreOf trailHead, ratingOf trailHead)
-    |> Seq.fold (fun acc (score, rating) -> fst acc + score, snd acc + rating) (0, 0)
+    |> Seq.fold
+        (fun acc trailHead ->
+            reachedFrom trailHead
+            |> (fun trails -> scoreOf trails, ratingOf trails)
+            |> (fun (score, rating) -> fst acc + score, snd acc + rating))
+        (0, 0)
 
-let topograficMap = Parsing.parse "Datasets/Day10_Sample.txt"
+let topograficMap = Parsing.parse "Datasets/Day10.txt"
 let score, rating = solve topograficMap
 
 printfn $"{score},{rating}"
